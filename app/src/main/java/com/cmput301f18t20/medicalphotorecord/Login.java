68088373e332dc5e78271643237b8736d340e59c
Michael Hackman <mwhackma@ualberta.ca>
2018-10-22 15:14:33 -0700

    Added onClickListeners to each element of UI

--- app/src/main/java/com/cmput301f18t20/medicalphotorecord/Login.java
+++ app/src/main/java/com/cmput301f18t20/medicalphotorecord/Login.java
@@ -2,12 +2,17 @@ package com.cmput301f18t20.medicalphotorecord;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
-    private Button LoginButton;
+    protected Button LoginButton;
+    protected Button SignUpButton;
+    protected EditText UserIDText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +20,36 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         LoginButton = findViewById(R.id.LoginButton);
+        SignUpButton = findViewById(R.id.SignUpButton);
+        UserIDText = findViewById(R.id.UserIDText);
 
-        /* sets background to a deep blue */
-        //getWindow().getDecorView().setBackgroundColor(0xff264a73);
-        //getWindow().getDecorView().setBackgroundColor(0xfffef501);
-
+        /* Needed for when a user focuses on the User ID text box */
+        /* clear default text if it's in the box */
+        UserIDText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    onUserIDClick(v);
+                }
+            }
+        });
     }
 
+    /**Run when the UserID box is brought into focus or clicked on
+     * @param view Unused as function only applies to one view anyways
+     */
+    public void onUserIDClick(View view) {
+        /* clear default text if it's in the box */
+        if (UserIDText.getText().toString().equals(
+                this.getText(R.string.userID_prompt).toString())) {
+            UserIDText.setText("");
+        }
+    }
+
+    /**Run when the Login button is clicked on.  On successful login, transitions to home screen
+     * for Provider or Patient.
+     * @param view Unused as function only applies to one view anyways
+     */
     public void onLoginClick(View view) {
         LoginButton.setBackgroundColor(0xFF476B00);
 
@@ -31,6 +59,22 @@ public class Login extends AppCompatActivity {
             @Override
             public void run() {
                 LoginButton.setBackgroundColor(0xFF669900);
+            }
+        }, 1000);
+    }
+
+    /**Run when the Sign up button is clicked on.  Transitions to sign up page.
+     * @param view Unused as function only applies to one view anyways
+     */
+    public void onSignUpClick(View view) {
+        SignUpButton.setBackgroundColor(0xFF00759C);
+
+        //wait 1 second
+        SignUpButton.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                SignUpButton.setBackgroundColor(0xFF0099CC);
             }
         }, 1000);
     }
