533d3bdc29b2b65f363f3c2e680883e48f9b96a7
Michael Hackman <mwhackma@ualberta.ca>
2018-10-29 23:22:43 -0600

    Even easier way to test they are the same

--- app/src/test/java/com/cmput301f18t20/medicalphotorecord/ProviderTests.java
+++ app/src/test/java/com/cmput301f18t20/medicalphotorecord/ProviderTests.java
@@ -85,16 +85,12 @@ public class ProviderTests {
         // If patient is there, check if they're the same patients
         provider.assignPatient(patient);
         Patient patientGot1 = provider.getPatient("0000");
+        Patient patientGot2 = provider.getPatient(0);
         assertEquals("compare patient userId", "0000", patientGot1.getUserID());
         assertEquals("compare patient email", "patient_email@email.com", patientGot1.getEmail());
         assertEquals("compare patient phone number", "1234567890", patientGot1.getPhoneNumber());
-
-        //do the same as above, but by index (for when they're clicked on)
-        patientGot1 = provider.getPatient(0);
-        assertEquals("compare patient userId", "0000", patientGot1.getUserID());
-        assertEquals("compare patient email", "patient_email@email.com", patientGot1.getEmail());
-        assertEquals("compare patient phone number", "1234567890", patientGot1.getPhoneNumber());
-
+        assertEquals("Got same object when fetched by index", patientGot1, patientGot2);
+        
     }
 
     @Test
