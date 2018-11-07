0a2ec9bcf744c2751b6727210cf3a0fd2763f64c
Michael Hackman <mwhackma@ualberta.ca>
2018-11-06 17:57:07 -0700

    Sign up click added

--- app/src/main/java/com/cmput301f18t20/medicalphotorecord/SignUp.java
+++ app/src/main/java/com/cmput301f18t20/medicalphotorecord/SignUp.java
@@ -2,15 +2,38 @@ package com.cmput301f18t20.medicalphotorecord;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class SignUp extends AppCompatActivity {
+    CheckBox PatientCheckBox, ProviderCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        PatientCheckBox = findViewById(R.id.PatientCheckBox);
+        ProviderCheckBox = findViewById(R.id.ProviderCheckBox);
+    }
 
+    public void onProviderClick(View v) {
+
+        /* if provider is checked, uncheck patient */
+        if (PatientCheckBox.isChecked()) {
+            PatientCheckBox.setChecked(false);
+        }
+    }
+
+    public void onPatientClick(View v) {
+
+        /* if patient is checked, uncheck provider */
+        if (ProviderCheckBox.isChecked()) {
+            ProviderCheckBox.setChecked(false);
+        }
+    }
+
+    public void onSaveClick(View v) {
         User user = new User("9944888855");
         user.setEmail("hello@gmail.com");
         user.setPhoneNumber("00778854545");
