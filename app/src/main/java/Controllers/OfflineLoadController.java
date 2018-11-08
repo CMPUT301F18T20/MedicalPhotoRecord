44d5e80a499a50a0dd32ee981503db54a7def06a
Michael Hackman <mwhackma@ualberta.ca>
2018-11-08 16:18:50 -0700

    implemented offline load and save controllers for sign up

--- app/src/main/java/Controllers/OfflineLoadController.java
+++ app/src/main/java/Controllers/OfflineLoadController.java
@@ -22,39 +22,30 @@ import static GlobalSettings.GlobalSettings.PROVIDERFILE;
 
 public class OfflineLoadController {
 
-    // Load from file into patient list
-    public static ArrayList<Patient> loadPatientList(Context context){
-        ArrayList<Patient> fileList = new ArrayList<Patient>();
+    private static ArrayList<User> loadFromDisk(String filename, Context context) {
+        ArrayList<User> fileList = new ArrayList<User>();
 
         try{
-            FileInputStream fis = context.openFileInput(PATIENTFILE);
+            FileInputStream fis = context.openFileInput(filename);
             InputStreamReader isr = new InputStreamReader(fis);
             BufferedReader reader = new BufferedReader(isr);
             Gson gson = new Gson();
-            Type listType = new TypeToken<ArrayList<Patient>>(){}.getType();
+            Type listType = new TypeToken<ArrayList<User>>(){}.getType();
             fileList = gson.fromJson(reader, listType);
         } catch (FileNotFoundException e){
             //TODO handle exception
             e.printStackTrace();
         }
-        return(fileList);
+        return fileList ;
+    }
+
+    // Load from file into patient list
+    public static ArrayList<User> loadPatientList(Context context){
+        return loadFromDisk(PATIENTFILE, context);
     }
 
     // Load from file into provider list
-    public static ArrayList<Provider> loadProviderList(Context context){
-        ArrayList<Provider> fileList = new ArrayList<Provider>();
-
-        try{
-            FileInputStream fis = context.openFileInput(PROVIDERFILE);
-            InputStreamReader isr = new InputStreamReader(fis);
-            BufferedReader reader = new BufferedReader(isr);
-            Gson gson = new Gson();
-            Type listType = new TypeToken<ArrayList<Provider>>(){}.getType();
-            fileList = gson.fromJson(reader, listType);
-        } catch (FileNotFoundException e){
-            //TODO handle exception
-            e.printStackTrace();
-        }
-        return(fileList);
+    public static ArrayList<User> loadProviderList(Context context){
+        return loadFromDisk(PROVIDERFILE, context);
     }
 }
