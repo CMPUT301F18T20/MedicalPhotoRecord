b8d07fa12dff7a3ea86cc31dd2e37f6205dd58d8
Michael Hackman <mwhackma@ualberta.ca>
2018-11-11 18:22:48 -0700

    Added Javadoc to Filter class.  Added copyright to all model files

--- app/src/main/java/com/cmput301f18t20/medicalphotorecord/User.java
+++ app/src/main/java/com/cmput301f18t20/medicalphotorecord/User.java
@@ -1,3 +1,15 @@
+/*
+ * Class name: User
+ *
+ * Version: Version 1.0
+ *
+ * Developed by members of CMPUT301F18T20 on Date: 11/11/18 6:21 PM
+ *
+ * Last Modified: 08/11/18 7:49 AM
+ *
+ * Copyright (c) 2018, CMPUT301F18T20, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behavior at University of Alberta
+ */
+
 package com.cmput301f18t20.medicalphotorecord;
 
 import Exceptions.UserIDMustBeAtLeastEightCharactersException;
