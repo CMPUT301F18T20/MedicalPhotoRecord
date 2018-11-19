8d38db86222022f035009c478f2d15891ecf11ca
Akazfu <akazfu@gmail.com>
2018-11-19 08:31:22 -0700

    added set time for set reminder, working on set date.

--- app/src/main/java/Activities/SetAlarm.java
+++ app/src/main/java/Activities/SetAlarm.java
@@ -9,13 +9,49 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TimePicker;
 import android.widget.Toast;
-
 import com.cmput301f18t20.medicalphotorecord.R;
 import com.cmput301f18t20.medicalphotorecord.RingAlarm;
-
 import java.util.Calendar;
 
 public class SetAlarm extends AppCompatActivity {
 
-    
+    TimePicker timePicker;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_alarm);
+
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+
+        findViewById(R.id.button_Alarm).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                if (android.os.Build.VERSION.SDK_INT >= 23) {
+                    calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH),
+                            timePicker.getHour(), timePicker.getMinute(), 0);
+                } else {
+                    calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH),
+                            timePicker.getCurrentHour(), timePicker.getCurrentMinute(), 0);
+                }
+
+
+                setAlarm(calendar.getTimeInMillis());
+            }
+        });
     }
+
+    private void setAlarm(long time) {
+
+        AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Intent i = new Intent(this, RingAlarm.class);
+
+        PendingIntent pi = PendingIntent.getBroadcast(this, 0, i, 0);
+
+        am.setRepeating(AlarmManager.RTC, time, AlarmManager.INTERVAL_DAY, pi);
+        Toast.makeText(this, "Alarm is set", Toast.LENGTH_SHORT).show();
+    }
+}
