14bc90881751617c0419cfc5ff646a22d80e4e42
Akazfu <akazfu@gmail.com>
2018-11-19 07:13:15 -0700

    added layout for reminder

---
+++ app/src/main/java/Activities/SetReminder.java
@@ -0,0 +1,16 @@
+package Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import com.cmput301f18t20.medicalphotorecord.R;
+
+public class SetReminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_reminder);
+    }
+
+}
