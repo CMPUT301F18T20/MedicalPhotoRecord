1ee7156f5e56571a824ea02d89200567c9d88aa2
Akazfu <akazfu@gmail.com>
2018-11-19 08:20:26 -0700

    created ui for set time and alarm

---
+++ app/src/main/java/Activities/SetAlarm.java
@@ -0,0 +1,21 @@
+package Activities;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.cmput301f18t20.medicalphotorecord.R;
+import com.cmput301f18t20.medicalphotorecord.RingAlarm;
+
+import java.util.Calendar;
+
+public class SetAlarm extends AppCompatActivity {
+
+    
+    }
--- app/src/main/java/Activities/SetReminder.java
+++ app/src/main/java/Activities/SetReminder.java
@@ -3,14 +3,21 @@ package Activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
+import android.view.View;
+
 import com.cmput301f18t20.medicalphotorecord.R;
 
-public class SetReminder extends AppCompatActivity {
+public class  SetReminder extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_reminder);
+    }
+
+    public void onSetTimeClick(View v){
+        Intent intent = new Intent(this, SetAlarm.class);
+        startActivity(intent);
     }
 
 }
