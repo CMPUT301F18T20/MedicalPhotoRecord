45cd39391099d5f1434920b49a895b7057fdeb03
tnguyen10 <tnguyen1@ualberta.ca>
2018-11-26 16:55:46 -0800

    slide show for problem's photos connected to app

--- app/src/main/java/Activities/SlideshowActivity.java
+++ app/src/main/java/Activities/SlideshowActivity.java
@@ -1,23 +1,30 @@
 package Activities;
 
+import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.cmput301f18t20.medicalphotorecord.R;
 
+import static GlobalSettings.GlobalSettings.PROBLEMIDEXTRA;
+
 public class SlideshowActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
-    private String recordUUID;
+    private String problemUUID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slideshow);
 
-        this.recordUUID ="tobedonelater2";
+        // Get problem uuid
+        Intent intent = getIntent();
+        this.problemUUID = intent.getStringExtra(PROBLEMIDEXTRA);
+
+        // Pass problem uuid to adapter to show slide show
         viewPager = findViewById(R.id.slide_show_view_pager_id);
-        this.viewPager.setAdapter(new SlideShowViewPagerAdapter(SlideshowActivity.this, this.recordUUID));
+        this.viewPager.setAdapter(new SlideShowViewPagerAdapter(SlideshowActivity.this, this.problemUUID));
     }
 }
