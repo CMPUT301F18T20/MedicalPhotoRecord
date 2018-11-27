17b0ac52ca247c2bbc70ac8b9cd8c6683f5c711a
deasisrj1 <rdeasis@ualberta.ca>
2018-11-27 16:00:46 -0700

    AddPatientController documentation

--- app/src/main/java/Controllers/AddPatientController.java
+++ app/src/main/java/Controllers/AddPatientController.java
@@ -6,11 +6,30 @@ import com.cmput301f18t20.medicalphotorecord.Patient;
 import com.cmput301f18t20.medicalphotorecord.Provider;
 
 import java.util.ArrayList;
-import java.util.concurrent.ExecutionException;
+
+/**
+ * AddPatientController
+ * Adds the given patient into the given provider's list of patients
+ * only if the given patientID is valid or not yet added to the list
+ *
+ * @author  Richard De Asis
+ * @version 1.0
+ * @since   2018-11-27
+ */
 
 public class AddPatientController {
     Provider provider = null;
     Patient patient = null;
+
+    /**
+     * addPatient
+     * This method gets the given provider and patient
+     * it verifies if the given patient is valid or not yet added to the provider's list of patients
+     * then adds the patient to the list.
+     * @param context
+     * @param providerID
+     * @param patientID
+     */
 
     public void addPatient(Context context, String providerID ,String patientID){
         provider = new ModifyProviderController().getProvider(context, providerID);
@@ -32,6 +51,17 @@ public class AddPatientController {
 
         new ModifyProviderController().saveModifiedProvider(context, provider, provider.getEmail(),provider.getPhoneNumber());
     }
+
+    /**
+     * checkIfPatientAlreadyInList
+     * given a valid patient object, this method checks the
+     * provider's list of patients and returns false
+     * if a patient is already in the list.
+     * true is returned if the patient does not exist in the list yet.
+     *
+     * @param patient - patient object
+     * @return
+     */
 
     public boolean checkIfPatientAlreadyInList(Patient patient){
         ArrayList<Patient> patients = provider.getPatients();
