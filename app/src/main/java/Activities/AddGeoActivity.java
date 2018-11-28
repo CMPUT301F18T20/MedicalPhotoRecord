54afe476fb5f1ccf63e85ab81088d412e8d92418
Akazfu <akazfu@gmail.com>
2018-11-28 13:13:12 -0700

    added geolocation object

--- app/src/main/java/Activities/AddGeoActivity.java
+++ app/src/main/java/Activities/AddGeoActivity.java
@@ -1,3 +1,14 @@
+/*
+ * Class name: GeoLocation
+ *
+ * Version: Version 1.0
+ *
+ * Developed by members of CMPUT301F18T20 on Date: 11/27/18 6:21 PM
+ *
+ * Last Modified: 11/27/18 7:50 AM
+ *
+ * Copyright (c) 2018, CMPUT301F18T20, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behavior at University of Alberta
+ */
 package Activities;
 
 import android.Manifest;
