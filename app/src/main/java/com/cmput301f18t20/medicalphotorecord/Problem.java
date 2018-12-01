b01ef72d61ee153ff3535219f43981aa2b7c9a95
tnguyen10 <tnguyen1@ualberta.ca>
2018-11-30 18:40:23 -0800

    problem javadoc

--- app/src/main/java/com/cmput301f18t20/medicalphotorecord/Problem.java
+++ app/src/main/java/com/cmput301f18t20/medicalphotorecord/Problem.java
@@ -32,100 +32,39 @@ public class Problem implements Serializable {
             dateCreated = new Date(System.currentTimeMillis()),
             dateLastModified = new Date(System.currentTimeMillis());
 
+    /**
+     * @param createdByUserID
+     * @param title
+     * @throws UserIDMustBeAtLeastEightCharactersException
+     * @throws TitleTooLongException
+     */
     public Problem(String createdByUserID, String title)
             throws UserIDMustBeAtLeastEightCharactersException, TitleTooLongException {
         this.setCreatedByUserID(createdByUserID);
         this.setTitle(title);
     }
 
+    /**
+     * UUID getter
+     * @return UUID of problem object
+     */
     public String getUUID() {
         return UUID;
     }
 
-    public Record getRecord(int recordIndex) {
-        return this.records.get(recordIndex);
-    }
-
-    public void setRecord(Record record, int recordIndex) {
-        this.records.set(recordIndex, record);
-    }
-
-    public ArrayList<Record> getRecords() {
-        return records;
-    }
-
-    public void addRecord(Record record) {
-        records.add(record);
-    }
-
-    public void removeRecord(Record record) {
-        records.remove(record);
-    }
-
-    public void removeRecord(int recordIndex) {
-        records.remove(recordIndex);
-    }
-
-    /* separates the patientRecords from the records and returns them */
-    public ArrayList<PatientRecord> getAllPatientRecords() {
-        ArrayList<PatientRecord> patientRecords = new ArrayList<>();
-
-        //iterate through all records stored
-        for (Record record : this.records) {
-
-            //if they are a patient record they contain photos, so add them to the patientRecords array
-            if (record.getClass() == PatientRecord.class) {
-                patientRecords.add((PatientRecord) record);
-            }
-        }
-
-        return patientRecords;
-    }
-
-    /* returns all photos for all records associated with the problem
-     * in order of added record.
+    /**
+     * descripion getter
+     * @return description
      */
-    public ArrayList<Photo> getAllPhotosFromRecordsInOrder() {
-        ArrayList<Photo> returnPhotoArray = new ArrayList<>();
-        ArrayList<PatientRecord> patientRecords = this.getAllPatientRecords();
-
-        //iterate through all patient records stored
-        for (PatientRecord record : patientRecords) {
-
-            //add all the photos in patient records
-            returnPhotoArray.addAll(record.getPhotos());
-        }
-
-        return returnPhotoArray;
-    }
-
-    /* returns all geo for all records associated with the problem
-     * in order of added record.
-     */
-    public ArrayList<Location> getAllGeoFromRecords() {
-        //TODO this should return a hashmap of HASHMAP<Location, PatientRecord>
-        //TODO so that we can display some information (like title) in the map view
-        ArrayList<Location> returnGeoArray = new ArrayList<>();
-        ArrayList<PatientRecord> patientRecords = this.getAllPatientRecords();
-
-        //iterate through all patient records stored
-        for (PatientRecord record : patientRecords) {
-
-            //add all the locations in patient records
-            returnGeoArray.add(record.getGeolocation());
-        }
-
-        return returnGeoArray;
-    }
-
-    public int getRecordCount() {
-        return records.size();
-    }
-
     public String getDescription() {
         return description;
     }
 
+    /**
+     * description setter
+     * @param description
+     * @throws ProblemDescriptionTooLongException > 300 characters
+     */
     public void setDescription(String description) throws ProblemDescriptionTooLongException {
         if (description.length() > 300){
             throw new ProblemDescriptionTooLongException();
@@ -133,10 +72,19 @@ public class Problem implements Serializable {
         this.description = description;
     }
 
+    /**
+     * userID getter
+     * @return createdByUserID
+     */
     public String getCreatedByUserID() {
         return createdByUserID;
     }
 
+    /**
+     * userID setter
+     * @param createdByUserID
+     * @throws UserIDMustBeAtLeastEightCharactersException
+     */
     public void setCreatedByUserID(String createdByUserID)
             throws UserIDMustBeAtLeastEightCharactersException {
         if (createdByUserID.length() >= 8) {
@@ -146,10 +94,19 @@ public class Problem implements Serializable {
         }
     }
 
+    /**
+     * title getter
+     * @return title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * title setter
+     * @param title
+     * @throws TitleTooLongException > 30 characters
+     */
     public void setTitle(String title) throws TitleTooLongException {
         if (title.length() > 30) {
             throw new TitleTooLongException();
@@ -157,22 +114,42 @@ public class Problem implements Serializable {
         this.title = title;
     }
 
+    /**
+     * date created getter
+     * @return dateCreated
+     */
     public Date getDate() {
         return dateCreated;
     }
 
+    /**
+     * date created setter
+     * @param dateCreated
+     */
     public void setDate(Date dateCreated) {
         this.dateCreated = dateCreated;
     }
 
+    /**
+     * date last modified getter
+     * @return dateLastModified
+     */
     public Date getDateLastModified() {
         return dateLastModified;
     }
 
+    /**
+     * date last modified setter
+     * @param dateLastModified
+     */
     public void setDateLastModified(Date dateLastModified) {
         this.dateLastModified = dateLastModified;
     }
 
+    /**
+     * for displaying problem object
+     * @return formated string: title
+     */
     public String toString(){
         return this.title;
     }
