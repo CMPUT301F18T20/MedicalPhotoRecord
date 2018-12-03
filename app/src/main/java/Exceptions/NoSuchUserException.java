4d2a436a372ba27a19bc3adaf9f0c3348a73de51
tnguyen10 <tnguyen1@ualberta.ca>
2018-12-02 19:03:05 -0800

    exception javadoc

--- app/src/main/java/Exceptions/NoSuchUserException.java
+++ app/src/main/java/Exceptions/NoSuchUserException.java
@@ -1,4 +1,11 @@
 package Exceptions;
 
+
+/**
+ * NoSuchUserException
+ * Thrown when user (patient or provider) is not found
+ * @version 2.0
+ * @see com.cmput301f18t20.medicalphotorecord.User
+ */
 public class NoSuchUserException extends Throwable {
 }
--- app/src/main/java/Exceptions/TitleTooLongException.java
+++ app/src/main/java/Exceptions/TitleTooLongException.java
@@ -1,4 +1,10 @@
 package Exceptions;
 
+/**
+ * TitleTooLongException
+ * Thrown when title > 30 characters
+ * @version 2.0
+ * @see com.cmput301f18t20.medicalphotorecord.Problem
+ */
 public class TitleTooLongException extends Throwable {
 }
